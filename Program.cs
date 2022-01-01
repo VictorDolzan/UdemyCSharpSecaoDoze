8acddd1363202e1aff398c18b53f52a305be991a
Victor Dolsan <victorddf5@gmail.com>
2022-01-01 13:35:43 -0300

    Foi criada a classe -Posicao-

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using CSharpSecapDoze.Tabuleiro;
 
 namespace CSharpSecapDoze
 {
@@ -11,7 +12,11 @@ namespace CSharpSecapDoze
     {
         public static void Main(string[] args)
         {
-            
+            Posicao P;
+
+            P = new Posicao(3 ,4);
+
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CSharpSecapDoze.Tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int externalLinha, int externalColuna)
+        {
+            linha = externalLinha;
+            coluna = externalColuna;
+        }
+
+        public override string ToString()
+        {
+            return linha
+            + ", "
+            + coluna;
+        }
+    }
+}
