337e01c983232afd76890c07f62c1a29cea03370
Victor Dolsan <victorddf5@gmail.com>
2022-01-01 21:10:39 -0300

    Foram inseridos os metodos -PodeMover- e -MovimentosPossiveis- na classe Torre. Ambos acompanhando a explicacao do professor do curso de C# da Udemy

--- Xadrez/Torre.cs
+++ Xadrez/Torre.cs
@@ -13,6 +13,67 @@ namespace CSharpSecapDoze.Xadrez
         public Torre(Tabuleiro ToTab, Cor ToCor) : base(ToTab, ToCor)
         {
         }
+
+       private bool PodeMover(Posicao pos)
+        {
+            Peca p = tab.RetornarPecaT(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.TabLinhas, tab.TabColunas];
+            
+            Posicao pos = new Posicao(0,0);
+
+            //ACIMA
+            pos.DefinirValores(posicao.linha -1, posicao.coluna);
+            while(tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if(tab.RetornarPecaT(pos) != null && tab.RetornarPecaT(pos).cor != this.cor)
+                {
+                    break;
+                } 
+                pos.linha = pos.linha - 1;
+            }
+            //Abaixo
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna);
+            while(tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if(tab.RetornarPecaT(pos) != null && tab.RetornarPecaT(pos).cor != this.cor)
+                {
+                    break;
+                } 
+                pos.linha = pos.linha + 1;
+            }
+            //Direita
+            pos.DefinirValores(posicao.linha, posicao.coluna + 1);
+            while(tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if(tab.RetornarPecaT(pos) != null && tab.RetornarPecaT(pos).cor != this.cor)
+                {
+                    break;
+                } 
+                pos.coluna = pos.coluna + 1;
+            }
+            //Esquerda
+            pos.DefinirValores(posicao.linha, posicao.coluna - 1);
+            while(tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if(tab.RetornarPecaT(pos) != null && tab.RetornarPecaT(pos).cor != this.cor)
+                {
+                    break;
+                } 
+                pos.coluna = pos.coluna - 1;
+            }
+
+            return mat;
+        }
+
         public override string ToString()
         {
             return "T";
