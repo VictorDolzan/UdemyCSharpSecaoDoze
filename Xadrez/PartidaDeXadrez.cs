62beb85fd996ccd7cda0e93dccb05114f8158892
Victor Dolsan <victorddf5@gmail.com>
2022-01-02 09:51:24 -0300

    Foram criados os metodos -ColocarNovaPeca-, -PecasCapturadas- e -PecasEmJogo- na classe PartidaDeXadrez, assim como foi atualizado o metodo -ExecutaMovimento-.

--- Xadrez/PartidaDeXadrez.cs
+++ Xadrez/PartidaDeXadrez.cs
@@ -10,10 +10,12 @@ namespace CSharpSecapDoze.Xadrez
 {
     class PartidaDeXadrez
     {
-        public Tabuleiro PDXTab{ get; private set; }
-        public int PDXTurno{ get; private set; }
-        public Cor JogadorAtual{ get; private set; }
+        public Tabuleiro PDXTab { get; private set; }
+        public int PDXTurno { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool Terminada { get; private set; }
+        private HashSet<Peca> HSpecas;
+        private HashSet<Peca> HSCapturadas;
 
         public PartidaDeXadrez()
         {
@@ -21,6 +23,8 @@ namespace CSharpSecapDoze.Xadrez
             PDXTurno = 1;
             JogadorAtual = Cor.Branca;
             Terminada = false;
+            HSpecas = new HashSet<Peca>();
+            HSCapturadas = new HashSet<Peca>();
             ColocarPecas();
         }
 
@@ -30,6 +34,12 @@ namespace CSharpSecapDoze.Xadrez
             p.IncrementarQuantidadeMovimentos();
             Peca pecaCapturada = PDXTab.RetirarPeca(destino);
             PDXTab.ColocarPeca(p, destino);
+            if(pecaCapturada != null)
+            {
+                HSCapturadas.Add(pecaCapturada);
+            }
+
+
         }
 
         public void RealizaJogada(Posicao origem, Posicao destino)
@@ -40,23 +50,23 @@ namespace CSharpSecapDoze.Xadrez
         }
         public void ValidarPosicaoDeOrigem(Posicao origem)
         {
-            if(PDXTab.RetornarPecaT(origem) == null)
+            if (PDXTab.RetornarPecaT(origem) == null)
             {
                 throw new TabuleiroException("Não existe peça na posição escolhida!");
 
             }
-            if(JogadorAtual != PDXTab.RetornarPecaT(origem).cor)
+            if (JogadorAtual != PDXTab.RetornarPecaT(origem).cor)
             {
                 throw new TabuleiroException("A peça de origem escolhida não é sua!");
             }
-            if(!PDXTab.RetornarPecaT(origem).ExistemMovimentosPossiveis())
+            if (!PDXTab.RetornarPecaT(origem).ExistemMovimentosPossiveis())
             {
                 throw new TabuleiroException("Não há movimentos possíveis para a peça de origem escolhida!");
             }
         }
         public void ValidarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if(!PDXTab.RetornarPecaT(origem).PodeMoverPara(destino))
+            if (!PDXTab.RetornarPecaT(origem).PodeMoverPara(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida!");
             }
@@ -64,7 +74,7 @@ namespace CSharpSecapDoze.Xadrez
 
         private void MudaJogador()
         {
-            if(JogadorAtual == Cor.Branca)
+            if (JogadorAtual == Cor.Branca)
             {
                 JogadorAtual = Cor.Preta;
             }
@@ -73,22 +83,51 @@ namespace CSharpSecapDoze.Xadrez
                 JogadorAtual = Cor.Branca;
             }
         }
+        public HashSet<Peca> PecasCapturadas(Cor cor)
+        {
+            HashSet<Peca> aux = new HashSet<Peca>();
+            foreach(Peca x in HSCapturadas)
+            {
+                if(x.cor == cor)
+                {
+                    aux.Add(x);
+                }
+            }
+            return aux;
+        }
+        public HashSet<Peca> PecasEmJogo(Cor cor)
+        {
+            HashSet<Peca> aux = new HashSet<Peca>();
+            foreach(Peca x in HSpecas)
+            {
+                if(x.cor == cor)
+                {
+                    aux.Add(x);
+                }
+            }
+            aux.ExceptWith(PecasCapturadas(cor));
+            return aux;
+        }
+        public void ColocarNovaPeca(char coluna, int linha, Peca peca)
+        {
+            PDXTab.ColocarPeca(peca, new PosicaoXadrez(coluna, linha).ToPosicao());
+            HSpecas.Add(peca);
+        }
         private void ColocarPecas()
         {
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Branca), new PosicaoXadrez('c', 1).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Branca), new PosicaoXadrez('c', 2).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Branca), new PosicaoXadrez('d', 2).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Branca), new PosicaoXadrez('e', 2).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Branca), new PosicaoXadrez('e', 1).ToPosicao());
-            PDXTab.ColocarPeca(new Rei(PDXTab, Cor.Branca), new PosicaoXadrez('d', 1).ToPosicao());
+            ColocarNovaPeca('c', 1, new Torre(PDXTab, Cor.Branca));
+            ColocarNovaPeca('c', 2, new Torre(PDXTab, Cor.Branca));
+            ColocarNovaPeca('d', 2, new Torre(PDXTab, Cor.Branca));
+            ColocarNovaPeca('e', 2, new Torre(PDXTab, Cor.Branca));
+            ColocarNovaPeca('e', 1, new Torre(PDXTab, Cor.Branca));
+            ColocarNovaPeca('d', 1, new Rei(PDXTab, Cor.Branca));
 
-             PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Preta), new PosicaoXadrez('c', 8).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Preta), new PosicaoXadrez('c', 7).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Preta), new PosicaoXadrez('d', 7).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Preta), new PosicaoXadrez('e', 7).ToPosicao());
-            PDXTab.ColocarPeca(new Torre(PDXTab, Cor.Preta), new PosicaoXadrez('e', 8).ToPosicao());
-            PDXTab.ColocarPeca(new Rei(PDXTab, Cor.Preta), new PosicaoXadrez('d', 8).ToPosicao());
-
+            ColocarNovaPeca('c', 7, new Torre(PDXTab, Cor.Preta));
+            ColocarNovaPeca('c', 8, new Torre(PDXTab, Cor.Preta));
+            ColocarNovaPeca('d', 7, new Torre(PDXTab, Cor.Preta));
+            ColocarNovaPeca('e', 7, new Torre(PDXTab, Cor.Preta));
+            ColocarNovaPeca('e', 8, new Torre(PDXTab, Cor.Preta));
+            ColocarNovaPeca('d', 8, new Rei(PDXTab, Cor.Preta));
 
         }
     }
